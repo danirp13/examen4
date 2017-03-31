55adfa6855f59eee7bd969c364cf7e1ac32238e9
daniel <danirsolarte@gmail.com>
2017-03-30 22:28:32 -0500

    nueva acctualizacion

--- estudiantes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from estudiantes.models import Estudiante, Materia
-# Register your models here.
-#admin.site.register(Category)
-#admin.site.register(Photo)
-admin.site.register(Estudiante)
-admin.site.register(Materia)
-# Register your models here.
